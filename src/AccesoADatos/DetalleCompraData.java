a154674db7b92e5234dc058201f75b39453ef468
Thiagojo08 <thiagotavarez015@gmail.com>
2023-11-11 12:06:38 -0300

    metodos para agregar detalles de compras

--- src/AccesoADatos/DetalleCompraData.java
+++ src/AccesoADatos/DetalleCompraData.java
@@ -1,7 +1,13 @@
 
 package AccesoADatos;
 
+import Entidades.DetalleCompra;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 
 public class DetalleCompraData {
@@ -13,5 +19,28 @@ public class DetalleCompraData {
  con = Conexion.getConexion();
  
  }
+     
+         public void GuardarDetalleDeCompras(DetalleCompra detallecompra) {
+        String sql = "INSERT INTO detallecompra(cantidad, preciocosto, compra, producto)"
+                + "VALUES(?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, detallecompra.getCantidad());
+            ps.setDouble(2, detallecompra.getPrecioCosto());
+            ps.setInt(3, detallecompra.getCompra().getIdCompra());
+            ps.setInt(4, detallecompra.getProducto().getIdProducto());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+
+                detallecompra.setIdDetalle(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Detalle de compra Agregado");
+            }
+            ps.close();
+
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla de detallesCompras");
+        }
     
 }
